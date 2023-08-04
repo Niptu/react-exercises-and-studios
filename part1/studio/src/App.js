49ae2d665cc8966dd71b023ab4497a5cdafafbdf
Nikolai <nikolaiptukhin@gmail.com>
2023-08-03 21:46:22 -0400

    exercise done

---
+++ part1/studio/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import RecipeDescription from '../src/components/Description.js';
+import RecipeIngredients from '../src/components/Ingredients.js';
+import RecipePhoto from '../src/components/Photos';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="recipePhotoBlock">
+        <RecipePhoto />
+        <div className="recipePhotoBlock">
+
+        <RecipeDescription />
+        <RecipeIngredients />
+
+     </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
