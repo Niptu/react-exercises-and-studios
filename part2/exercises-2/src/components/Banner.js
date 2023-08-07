59a40e428069710d2a59996d8940cc9545944b1d
Nikolai <nikolaiptukhin@gmail.com>
2023-08-07 13:04:46 -0400

    Exercise

--- part2/exercises-2/src/components/Banner.js
+++ part2/exercises-2/src/components/Banner.js
@@ -2,7 +2,7 @@ import './styles.css';
 
 function Banner(){
    return(
-      <h1>Hello, World!</h1>
+      <h1 calssName="header">The Amazing Ocean</h1>
    );
 }
 
--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,12 +1,19 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
-
+const listItem = oceans.map(ocean =>
+   <div key={ocean.id} className="profile">
+      <img scr = {ocean.image} alt={ocean.name} className = "img"/>
+   </div>
+   );
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+       <ul>
+         {listItem}
+       </ul>
          <Button />
       </>  
    );
